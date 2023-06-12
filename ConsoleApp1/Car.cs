c2f660315649a508c6a58475edd26e34e45c5bdb
Kantapat Kowadisai (NTL) <CF304591@tidlor.com>
2023-06-12 14:33:24 +0700

    init my app

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        //property
+        private string brand { get; set; } 
+        private string model { get; set; }
+        private double price { get; set; }
+        private int year { get; set; }
+
+        //constructor
+        public Car(string brand, string model, double price, int year)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.price = price;
+            this.year = year;
+        }
+
+        //method
+        public void Whoami() {
+            //do it
+            Console.WriteLine($"This is a {brand} : {model}");
+        }
+        public double Getprice() { 
+            return price;
+        }
+        public double Gettax()
+        {
+            return (price * 7)/100;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World! Mr.Kantapat Kowadisai");
+Console.WriteLine(".Netcore 7.0");
+Console.WriteLine();
+
+var car1 = new Car("Porche", "x1", 15000000.00, 2020);
+var car2 = new Car("Honda", "jass", 600000.00, 2018);
+
+car1.Whoami();
+var pricecar1 = car1.Getprice();
+Console.WriteLine($"price : {pricecar1}");
+var taxcar1 = car1.Gettax();
+Console.WriteLine($"tax : {taxcar1}");
+Console.WriteLine();
+
+car2.Whoami();
+var pricecar2 = car2.Getprice();
+Console.WriteLine($"price : {pricecar2}");
+var taxcar2 = car2.Gettax();
+Console.WriteLine($"tax : {taxcar2}");
+Console.WriteLine();
